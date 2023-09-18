1d3667cf02fed8277dce9db82f154180b79fea9c
Ammar <33799463+ammar629@users.noreply.github.com>
2023-09-18 20:54:44 +0500

    Added a utility StatelessWidget called SImpleTextWidgetUtil
    * Implemented a utility which will take care of all the parameters like name, textColor,letterSpacing and fontSize for the Text() widget
    
    * Applied formatting to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ void main() {
   ));
 }
 
-class NinjaCard extends StatelessWidget{
+class NinjaCard extends StatelessWidget {
   const NinjaCard({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
@@ -18,6 +18,44 @@ class NinjaCard extends StatelessWidget{
         backgroundColor: Colors.grey[850],
         elevation: 0,
         centerTitle: true,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(children: <Widget>[
+          SimpleTextWidgetUtil(
+            'NAME',
+             Colors.grey, 2.0,
+              null,
+               FontWeight.normal
+               ),
+
+          SimpleTextWidgetUtil('Shinobi',Colors.amberAccent,2.0,28.0,FontWeight.bold),
+        ]),
+      ),
+    );
+  }
+}
+
+class SimpleTextWidgetUtil extends StatelessWidget {
+  final String name;
+  final Color textColor;
+  final double textLetterSpacing;
+  final double? textFontSize;
+  final FontWeight textFontWeight;
+
+  const SimpleTextWidgetUtil(this.name, this.textColor, this.textLetterSpacing,
+      this.textFontSize, this.textFontWeight,
+      {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      name,
+      style: TextStyle(
+        color: textColor,
+        letterSpacing: textLetterSpacing,
+        fontSize: textFontSize ?? 14,
+        fontWeight: textFontWeight,
       ),
     );
   }
