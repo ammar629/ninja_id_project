c21b60a301c23a8e9329a3d5781bd6e9afb647e6
Ammar <33799463+ammar629@users.noreply.github.com>
2023-09-18 14:45:07 +0500

    Changed the AppBar.backgroundColor property to red[600]

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class NinjaCard extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: const Text('Ninja ID Card'),
+        backgroundColor: Colors.red[600],
       ),
     );
   }
